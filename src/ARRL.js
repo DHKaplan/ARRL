3221b14325e561dd5744d5c502a821c75ac16156
DHKaplan <wa1oui.ham@gmail.com>
2015-06-11 11:49:42 -0500

    Fixed Aplite no BT indicator

--- src/ARRL.js
+++ src/ARRL.js
@@ -9,8 +9,8 @@ Pebble.addEventListener('webviewclosed',
   { 
   console.log("in webviewclosed");
   var dict = JSON.parse(decodeURIComponent(e.response));
-    
-  //Send a string to Pebble
+
+    //Send a string to Pebble
   Pebble.sendAppMessage(dict, 
         function(e) 
             { 
