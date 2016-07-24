b44bd01fe82323d3d5efe50087510b5468ecb6e0
DHKaplan <wa1oui.ham@gmail.com>
2016-07-23 21:41:34 -0500

    Version 6.0: Added Clay Config page
    Also Coding Enhancements

--- src/ARRL.c
+++ src/ARRL.c
@@ -1,7 +1,15 @@
 #include "pebble.h"
 
-#define DATE_STYLE 0
-#define BT_VIBRATE 1  
+#include "ARRL.h"
+
+#include "handle_bluetooth.h"
+
+#include "BTLine_update_callback.h"
+
+#include "Battery.h"
+
+#include "Callbacks.h"
+
 
 Window *window;
 
@@ -9,261 +17,43 @@ TextLayer *text_dayname_layer;
 TextLayer *text_mmdd_layer;
 TextLayer *text_year_layer;
 TextLayer *text_time_layer;
-TextLayer *text_battery_layer;
-
-Layer       *linelayer;
-Layer       *BTLayer;
 
 GFont        fontHelvNewLight20;
-GFont		     fontRobotoCondensed25;
+GFont        fontRobotoCondensed25;
 GFont        fontSystemGothicBold28;
 GFont        fontRobotoBoldSubset40;
 GFont        fontRobotoBoldSubset49;
 
-static GBitmap     *image;
-static BitmapLayer *image_layer;
-
-static int  batterychargepct;
-static int  BatteryVibesDone = 0;
-static int  batterycharging=0;
+ GBitmap     *image;
+ BitmapLayer *image_layer;
 
 GPoint     Linepoint;
-static int BTConnected = 1;
-static int BTVibesDone = 0;
-static char VibOnBTLoss[] = "0"; //From Config Page
 
-static int FirstTime = 0;
+//Common Defines
 
-static char date_type[]="us  ";
-static char dayname_text[] = "XXX";
-static char time_text[] = "00:00";
-static char mmdd_text[] = "XXXX00";
-static char year_text[] = "0000";
-static char seconds_text[] = "00";
+int       BTConnected = 1;
+int       BTVibesDone = 0;
 
-static char date_format[]="%b %e, %Y";
+Layer     *BatteryLineLayer;
+Layer     *BTLayer;
 
-GColor TextColorHold;
-GColor BGColorHold;
+TextLayer *text_battery_layer;
 
-void line_layer_update_callback(Layer *LineLayer, GContext* ctx) {
+int       PersistBTLoss         =  0;     // 0 = No Vib, 1 = Vib
+char      PersistDateFormat[]   = "0";    // 0 = US, 1 = Intl
 
-     graphics_context_set_fill_color(ctx, TextColorHold);
-     graphics_fill_rect(ctx, layer_get_bounds(LineLayer), 3, GCornersAll);
+ char     date_format[]="%b %e, %Y";
 
-     if (batterycharging == 1) {
-       #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorBlue);
-       #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
-       #endif
+//End Common
 
-       graphics_fill_rect(ctx, GRect(2, 1, 100, 4), 3, GCornersAll);
+ int FirstTime = 0;
 
-     } else if (batterychargepct > 20) {
-       #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorGreen);
-       #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
-       #endif
-
-       graphics_fill_rect(ctx, GRect(2, 1, batterychargepct, 4), 3, GCornersAll);
-
-     } else {
-      #ifdef PBL_COLOR
-          graphics_context_set_fill_color(ctx, GColorRed);
-       #else
-          graphics_context_set_fill_color(ctx, GColorBlack);
-       #endif
-
-       graphics_fill_rect(ctx, GRect(2, 1, batterychargepct, 4), 3, GCornersAll);
-     }
-}
-
-void handle_battery(BatteryChargeState charge_state) {
-  static char BatteryPctTxt[] = "+100%";
-
-  batterychargepct = charge_state.charge_percent;
-
-  if (charge_state.is_charging) {
-    batterycharging = 1;
-  } else {
-    batterycharging = 0;
-  }
-
-  // Reset if Battery > 20% ********************************
-  if (batterychargepct > 20) {
-     if (BatteryVibesDone == 1) {     //OK Reset to normal
-         BatteryVibesDone = 0;
-     }
-     #ifdef PBL_COLOR
-         text_layer_set_background_color(text_battery_layer, BGColorHold);
-         text_layer_set_text_color(text_battery_layer, TextColorHold);
-     #endif
-  }
-
-  //
-  if (batterychargepct < 30) {
-     if (BatteryVibesDone == 0) {            // Do Once
-         BatteryVibesDone = 1;
-         vibes_long_pulse();
-      }
-  }
-
-   if (charge_state.is_charging) {
-     strcpy(BatteryPctTxt, "Chrg");
-  } else {
-     snprintf(BatteryPctTxt, 5, "%d%%", charge_state.charge_percent);
-  }
-   text_layer_set_text(text_battery_layer, BatteryPctTxt);
-
-  //layer_mark_dirty(line_layer);
-}
-
-void handle_bluetooth(bool connected) {
-    if (connected) {
-         BTConnected = 1;     // Connected
-         BTVibesDone = 0;
-
-    } else {
-         BTConnected = 0;      // Not Connected
-
-         if ((BTVibesDone == 0) && (strcmp(VibOnBTLoss,"0") == 0)) {    
-             BTVibesDone = 1;
-             vibes_long_pulse();
-         }
-    }
-    layer_mark_dirty(BTLayer);
-}
-
-//BT Logo Callback;
-void BTLine_update_callback(Layer *BTLayer, GContext* BT1ctx) {
-
-       GPoint BTLinePointStart;
-       GPoint BTLinePointEnd;
-
-        //Default Colors
-        #ifdef PBL_COLOR
-           graphics_context_set_stroke_color(BT1ctx, TextColorHold);
-           graphics_context_set_fill_color(BT1ctx, BGColorHold);
-           graphics_fill_rect(BT1ctx, layer_get_bounds(BTLayer), 0, GCornerNone);
-        #else
-            graphics_context_set_stroke_color(BT1ctx, GColorWhite);
-            graphics_context_set_fill_color(BT1ctx, GColorBlack);
-            graphics_fill_rect(BT1ctx, layer_get_bounds(BTLayer), 0, GCornerNone);
-        #endif
-          
-        if (BTConnected == 0) {
-
-        #ifdef PBL_COLOR
-            graphics_context_set_stroke_color(BT1ctx, GColorRed);
-            graphics_context_set_fill_color(BT1ctx, TextColorHold);
-            graphics_fill_rect(BT1ctx, layer_get_bounds(BTLayer), 0, GCornerNone);
-        #else
-            graphics_context_set_stroke_color(BT1ctx, GColorBlack);
-            graphics_context_set_fill_color(BT1ctx, GColorWhite);
-            graphics_fill_rect(BT1ctx, layer_get_bounds(BTLayer), 0, GCornerNone);
-        #endif
-
-        // "X"" Line 1
-          BTLinePointStart.x = 1;
-          BTLinePointStart.y = 1;
-
-          BTLinePointEnd.x = 20;
-          BTLinePointEnd.y = 20;
-          graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-         // "X"" Line 2
-          BTLinePointStart.x = 1;
-          BTLinePointStart.y =20;
-
-          BTLinePointEnd.x = 20;
-          BTLinePointEnd.y = 1;
-          graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-      } else {
-
-       //Line 1
-       BTLinePointStart.x = 10;
-       BTLinePointStart.y = 1;
-
-       BTLinePointEnd.x = 10;
-       BTLinePointEnd.y = 20;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 1a
-       BTLinePointStart.x = 11;
-       BTLinePointStart.y = 1;
-
-       BTLinePointEnd.x = 11;
-       BTLinePointEnd.y = 20;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 2
-       BTLinePointStart.x = 10;
-       BTLinePointStart.y = 1;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 6;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 2a
-       BTLinePointStart.x = 11;
-       BTLinePointStart.y = 1;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 5;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 3
-       BTLinePointStart.x = 4;
-       BTLinePointStart.y = 5;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 15;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 3a
-       BTLinePointStart.x = 4;
-       BTLinePointStart.y = 6;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 16;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 4
-       BTLinePointStart.x = 4;
-       BTLinePointStart.y = 15;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 5;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 4a
-       BTLinePointStart.x = 4;
-       BTLinePointStart.y = 16;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 6;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 5
-       BTLinePointStart.x = 10;
-       BTLinePointStart.y = 20;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 15;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-
-       //Line 5a
-       BTLinePointStart.x = 11;
-       BTLinePointStart.y = 20;
-
-       BTLinePointEnd.x = 17;
-       BTLinePointEnd.y = 16;
-       graphics_draw_line(BT1ctx, BTLinePointStart, BTLinePointEnd);
-      }
-}
+ char date_type[]="us  ";
+ char dayname_text[] = "XXX";
+ char time_text[] = "00:00";
+ char mmdd_text[] = "XXXX00";
+ char year_text[] = "0000";
+ char seconds_text[] = "00";
 
 void handle_appfocus(bool in_focus){
     if (in_focus) {
@@ -317,9 +107,10 @@ if (units_changed & DAY_UNIT) {
 void handle_deinit(void) {
   tick_timer_service_unsubscribe();
   
-  persist_write_string(DATE_STYLE, date_type);
-  persist_write_string(BT_VIBRATE, VibOnBTLoss);
-
+  persist_write_string(MESSAGE_KEY_DATE_FORMAT_KEY,       PersistDateFormat);
+  persist_write_int(MESSAGE_KEY_BT_VIBRATE_KEY,           PersistBTLoss);
+  persist_write_int(MESSAGE_KEY_LOW_BATTERY_KEY,          PersistLow_Batt);
+  
   battery_state_service_unsubscribe();
   bluetooth_connection_service_unsubscribe();
   app_focus_service_unsubscribe();
@@ -330,7 +121,7 @@ void handle_deinit(void) {
   text_layer_destroy(text_dayname_layer);
   text_layer_destroy(text_battery_layer);
 
-  layer_destroy(linelayer);
+  layer_destroy(BatteryLineLayer);
   layer_destroy(BTLayer);
 
   fonts_unload_custom_font(fontHelvNewLight20);
@@ -344,76 +135,12 @@ void handle_deinit(void) {
   bitmap_layer_destroy(image_layer);
 }
 
-//Receive Input from Config html page:
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  //APP_LOG(APP_LOG_LEVEL_INFO, "In Inbox received callback");
-
-  char BTVibeConfig[] = "0";
-
-  FirstTime = 0;
-
-  // Read first item
-  Tuple *t = dict_read_first(iterator);
-
-  while(t != NULL) {
-
-    // Which key was received?
-    switch(t->key) {
-    case 0:
-      strcpy(date_type, t->value->cstring);
-
-      if (strcmp(date_type, "us") == 0) {
-         strcpy(date_format, "%b %d");
-      } else {
-         strcpy(date_format, "%d %b");
-      }
-
-      text_layer_set_text(text_mmdd_layer, mmdd_text);
-      break;
-      
-    case 1:
-      strcpy(BTVibeConfig, t->value->cstring); 
-      if (strcmp(BTVibeConfig, "0") == 0) {
-         strcpy(VibOnBTLoss,"0");
-      } else {
-         strcpy(VibOnBTLoss,"1");
-      }
-      break;
-      
-    default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-      break;
-    }
-
-    // Look for next item
-    t = dict_read_next(iterator);
-    }
-  }
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Inbox Message dropped!");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
-}
 
 
 void handle_init(void) {
-  GColor BGCOLOR   = COLOR_FALLBACK(GColorOxfordBlue, GColorBlack);
-  BGColorHold = BGCOLOR;
-
-  GColor TEXTCOLOR = COLOR_FALLBACK(GColorYellow, GColorWhite);
-  TextColorHold = TEXTCOLOR;
-
-
   window = window_create();
   window_stack_push(window, true /* Animated */);
-  window_set_background_color(window, BGCOLOR);
+  window_set_background_color(window, GColorOxfordBlue);
 
   Layer *window_layer = window_get_root_layer(window);
 
@@ -430,7 +157,7 @@ void handle_init(void) {
   app_message_register_outbox_sent(outbox_sent_callback);
 
   // Open AppMessage
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+  app_message_open(64, 64);
 
   // Dayname
   #ifdef PBL_PLATFORM_CHALK
@@ -439,8 +166,8 @@ void handle_init(void) {
       text_dayname_layer = text_layer_create(GRect(59, 25, 91, 40));
   #endif 
     
-  text_layer_set_text_color(text_dayname_layer,TEXTCOLOR);
-  text_layer_set_background_color(text_dayname_layer, BGCOLOR);
+  text_layer_set_text_color(text_dayname_layer,GColorYellow);
+  text_layer_set_background_color(text_dayname_layer, GColorOxfordBlue);
   text_layer_set_text_alignment(text_dayname_layer, GTextAlignmentCenter);
   text_layer_set_font(text_dayname_layer, fontSystemGothicBold28);
   layer_add_child(window_layer, text_layer_get_layer(text_dayname_layer));
@@ -452,8 +179,8 @@ void handle_init(void) {
        text_mmdd_layer = text_layer_create(GRect(59, 55, 91, 40));
   #endif
     
-  text_layer_set_text_color(text_mmdd_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_mmdd_layer, BGCOLOR);
+  text_layer_set_text_color(text_mmdd_layer, GColorYellow);
+  text_layer_set_background_color(text_mmdd_layer, GColorOxfordBlue);
   text_layer_set_text_alignment(text_mmdd_layer, GTextAlignmentCenter);
   text_layer_set_font(text_mmdd_layer, fontRobotoCondensed25);
   layer_add_child(window_layer, text_layer_get_layer(text_mmdd_layer));
@@ -466,8 +193,8 @@ void handle_init(void) {
   #endif
  
   text_layer_set_text_alignment(text_year_layer, GTextAlignmentCenter);  
-  text_layer_set_text_color(text_year_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_year_layer, BGCOLOR); 
+  text_layer_set_text_color(text_year_layer, GColorYellow);
+  text_layer_set_background_color(text_year_layer, GColorOxfordBlue); 
   text_layer_set_font(text_year_layer, fontRobotoCondensed25);
   layer_add_child(window_layer, text_layer_get_layer(text_year_layer));
 
@@ -485,24 +212,40 @@ void handle_init(void) {
   layer_add_child(window_layer, bitmap_layer_get_layer(image_layer));
 
   //Persistent Value Date Format:
-  if (persist_exists(DATE_STYLE)) {
-       persist_read_string(DATE_STYLE, date_type, sizeof(date_type));
+  if (persist_exists(MESSAGE_KEY_DATE_FORMAT_KEY)) {
+     persist_read_string(MESSAGE_KEY_DATE_FORMAT_KEY  , PersistDateFormat, sizeof(PersistDateFormat));
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set Date Format to                    %s - Persistent - 0 = US, 1 = Int'l", PersistDateFormat);
   }  else {
-       strcpy(date_type, "us");
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set Date Format to                    0 - Default - US");
+     strcpy(PersistDateFormat, "0"); //Default
   }
 
-  if (strcmp(date_type, "us") == 0) {
-      strcpy(date_format, "%b %d");
+  if (strcmp(PersistDateFormat, "0") == 0) {     // US
+     strcpy(date_format, "%b %e %Y");
   } else {
-      strcpy(date_format, "%d %b");
+     strcpy(date_format, "%e %b %Y");
   }
 
-  //Persistent Value VibOnBTLoss
-  if(persist_exists(BT_VIBRATE)) {
-     persist_read_string(BT_VIBRATE, VibOnBTLoss, sizeof(VibOnBTLoss));  
+  //Persistent Value Vib On BTLoss
+  if(persist_exists(MESSAGE_KEY_BT_VIBRATE_KEY)) {
+     PersistBTLoss = persist_read_int(MESSAGE_KEY_BT_VIBRATE_KEY);
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set BT Vibrate To                     %d - Persistent - 0 = NO Vib, 1 = Vib", PersistBTLoss);
   }  else {
-     strcpy(VibOnBTLoss, "0"); // Default
-  } 
+     PersistBTLoss = 0; // Default
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set BT Vibrate To                     0 - Default - No Vibrate");
+
+  }
+
+  //Persistent Value Vib on Low Batt
+  if(persist_exists(MESSAGE_KEY_LOW_BATTERY_KEY)) {
+     PersistLow_Batt = persist_read_int(MESSAGE_KEY_LOW_BATTERY_KEY);
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set Low Batt Vibrate To               %d - Persistent - 0 = NO Vib, 1 = Vib", PersistLow_Batt);
+  }  else {
+     PersistLow_Batt = 0; // Default
+     APP_LOG(APP_LOG_LEVEL_INFO, "    Set Low Batt Vibrate To               0 - Default - No Vibrate");
+
+  }
+
   
   // Time of Day is here
   #ifdef PBL_PLATFORM_CHALK
@@ -513,8 +256,8 @@ void handle_init(void) {
       text_layer_set_font(text_time_layer, fontRobotoBoldSubset49);
   #endif
     
-  text_layer_set_text_color(text_time_layer, TEXTCOLOR);
-  text_layer_set_background_color(text_time_layer, BGCOLOR);
+  text_layer_set_text_color(text_time_layer, GColorYellow);
+  text_layer_set_background_color(text_time_layer, GColorOxfordBlue);
   text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
@@ -525,9 +268,9 @@ void handle_init(void) {
       GRect line_frame = GRect(22, 122, 104, 6);
   #endif
     
-  linelayer = layer_create(line_frame);
-  layer_set_update_proc(linelayer, line_layer_update_callback);
-  layer_add_child(window_layer, linelayer);
+  BatteryLineLayer = layer_create(line_frame);
+  layer_set_update_proc(BatteryLineLayer, battery_line_layer_update_callback);
+  layer_add_child(window_layer, BatteryLineLayer);
 
   tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
 
@@ -549,8 +292,8 @@ void handle_init(void) {
         text_battery_layer = text_layer_create(GRect(85,2,55,28));
     #endif
       
-    text_layer_set_text_color(text_battery_layer, TEXTCOLOR);
-    text_layer_set_background_color(text_battery_layer, BGCOLOR);
+    text_layer_set_text_color(text_battery_layer, GColorYellow);
+    text_layer_set_background_color(text_battery_layer, GColorOxfordBlue);
     text_layer_set_text_alignment(text_battery_layer, GTextAlignmentRight);
     text_layer_set_font(text_battery_layer, fontHelvNewLight20);
 
